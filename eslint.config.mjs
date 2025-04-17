9386ee6b63ba3a8cc3787dbebad9087fb89dd9e4
vorotamoroz <vrtmrz@gmail.com>
2025-04-17 13:07:00 +0100

    ### Digging the weeds
    -   Many dependencies are updated to the latest version. And also the Svelte compiler is updated to the latest version.
    -   Now `npm run check` implemented in the package.json. Runs tsc, eslint, svelte-check at once.

---
+++ eslint.config.mjs
@@ -0,0 +1,75 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import svelte from "eslint-plugin-svelte";
+import _import from "eslint-plugin-import";
+import { fixupPluginRules } from "@eslint/compat";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
+});
+
+export default [
+	{
+		ignores: ["**/node_modules/**", "**/dist/**", "**/build/**","**/*.config.*","*.js"],
+	},
+	...compat.extends(
+		"eslint:recommended",
+		"plugin:@typescript-eslint/eslint-recommended",
+		"plugin:@typescript-eslint/recommended"
+	),
+	{
+		plugins: {
+			"@typescript-eslint": typescriptEslint,
+			svelte,
+			import: fixupPluginRules(_import),
+		},
+
+		languageOptions: {
+			parser: tsParser,
+			ecmaVersion: 5,
+			sourceType: "module",
+
+			parserOptions: {
+				project: ["tsconfig.json"],
+			},
+		},
+
+		rules: {
+			"no-unused-vars": "off",
+
+			"@typescript-eslint/no-unused-vars": [
+				"error",
+				{
+					args: "none",
+				},
+			],
+
+			"no-unused-labels": "off",
+			"@typescript-eslint/ban-ts-comment": "off",
+			"no-prototype-builtins": "off",
+			"@typescript-eslint/no-empty-function": "off",
+			"require-await": "error",
+			"@typescript-eslint/require-await": "warn",
+			"@typescript-eslint/no-misused-promises": "warn",
+			"@typescript-eslint/no-floating-promises": "warn",
+			"no-async-promise-executor": "warn",
+			"@typescript-eslint/no-explicit-any": "off",
+			"@typescript-eslint/no-unnecessary-type-assertion": "error",
+
+			"no-constant-condition": [
+				"error",
+				{
+					checkLoops: false,
+				},
+			],
+		},
+	},
+];
